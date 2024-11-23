33467a59f8234a9cc2be4c961eee1fc57126d249
Faiez Fattah Wisnubrata <30339866+faiezfattah@users.noreply.github.com>
2024-11-23 12:57:04 +0700

    fix lizard, rider ide support
    we love jetbrains :3

--- Assets/Enemies/Lizard/Lizard.cs
+++ Assets/Enemies/Lizard/Lizard.cs
@@ -1,3 +1,4 @@
+using DG.Tweening;
 using UnityEngine;
 using UnityEngine.Assertions;
 
@@ -5,38 +6,73 @@ public class Lizard : MonoBehaviour
 {
     [SerializeField]
     private Transform headPivotTransform;
+    [SerializeField] private float idleRotationDuration = 0.5f;
 
-    private bool isPlayerInTrigger = false;
-    private Transform playerTransform;
+    private static Transform _playerTransform;
+    private Tweener _currentTween;
 
     private void Awake()
     {
         Assert.IsNotNull(headPivotTransform);
 
-        playerTransform = GameObject.FindGameObjectWithTag("Player").transform;
+        _playerTransform = GameObject.FindGameObjectWithTag("Player").transform;
+        RotateIdle();
     }
+    
+    // private void Update()
+    // {
+    //     
+    //     
+    //     // float currentAngle = Mathf.Atan2(headPivotTransform.transform.localPosition.y, headPivotTransform.transform.localPosition.x) * Mathf.Rad2Deg;
+    //     // float angleDifference = Mathf.DeltaAngle(currentAngle, targetAngle);
+    //     //headPivotTransform.transform.Rotate(Vector3.forward, Time.deltaTime * 10);
+    //     // ReSharper disable once Unity.PerformanceCriticalCodeInvocation
+    //     //Debug.Log(targetRotation);
+    //     
+    //     // code from turret game
+    //     // Vector3 direction = target.position - transform.position;
+    //     // float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg - 180f;
+    //     // Quaternion targetRotation = Quaternion.AngleAxis(angle, Vector3.forward);
+    //     // transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, data.rotationSpeed * Time.deltaTime);
+    //     // 1. get directional vector 
+    //     // 2. get angle diff 
+    //     // 3. make quaternion cuz unity succ
+    //     // 4. apply quaternion
+    // }
 
-    // Update is called once per frame
-    void Update()
-    {
-        Vector3 directionToPlayer = playerTransform.position - headPivotTransform.position;
+    private void RotateIdle() {
+        // Vector3 directionToPlayer = (_playerTransform.position - headPivotTransform.position).normalized;
+        // float targetAngle = Mathf.Atan2(directionToPlayer.y, directionToPlayer.x) * Mathf.Rad2Deg;
+        // targetAngle = Mathf.Clamp(targetAngle, -90f, 90f);
+        Quaternion targetRotation = Quaternion.AngleAxis(headPivotTransform.rotation.z + 180f, Vector3.forward);
+        
+        _currentTween?.Kill();
+        
+        _currentTween = headPivotTransform.DORotateQuaternion(targetRotation, idleRotationDuration)
+                          .SetEase(Ease.Linear)
+                          .SetLoops(-1, LoopType.Yoyo);
+    }
+    private void RotateToPlayer() {
+        Vector3 directionToPlayer = (_playerTransform.position - headPivotTransform.position).normalized;
         float targetAngle = Mathf.Atan2(directionToPlayer.y, directionToPlayer.x) * Mathf.Rad2Deg;
-        float currentAngle = Mathf.Atan2(headPivotTransform.transform.localPosition.y, headPivotTransform.transform.localPosition.x) * Mathf.Rad2Deg;
-        float angleDifference = Mathf.DeltaAngle(currentAngle, targetAngle);
+        targetAngle = Mathf.Clamp(targetAngle, -180, 0f);
+        Quaternion targetRotation = Quaternion.AngleAxis(targetAngle, Vector3.forward);
+        
+        _currentTween?.Kill();
 
-        headPivotTransform.transform.Rotate(Vector3.forward, Time.deltaTime * 10);
-        Debug.Log(angleDifference);
+        _currentTween = headPivotTransform.DORotateQuaternion(targetRotation, 0.1f)
+                                            .SetEase(Ease.Linear);
     }
 
-    private void OnTriggerEnter(Collider other)
+    private void OnTriggerStay(Collider other)
     {
         if (other.CompareTag("Player"))
-            isPlayerInTrigger = true;
+            RotateToPlayer();
     }
 
     private void OnTriggerExit(Collider other)
     {
         if (other.CompareTag("Player"))
-            isPlayerInTrigger = false;
+            RotateIdle();
     }
 }
