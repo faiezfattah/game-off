07f34adb6fa7d19249ab8b3622bcb643d873aaa5
rfi <rfi@cunny.my.id>
2024-11-30 18:45:46 +0700

    dialogues SOs

---
+++ Assets/Dialogue/DialogueSystem/DialogueItem.cs
@@ -0,0 +1,8 @@
+using System;
+
+[Serializable]
+public class DialogueItem
+{
+    public DialogueSpeaker dialogueSpeaker;
+    public string text;
+}
---
+++ Assets/Dialogue/DialogueSystem/DialogueSpeaker.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "DialogueSpeaker", menuName = "Scriptable Objects/Dialogue/Dialogue Speaker")]
+public class DialogueSpeaker : ScriptableObject
+{
+    public string displayName;
+}
